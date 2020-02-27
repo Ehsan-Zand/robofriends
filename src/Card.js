b5624427d60bf9cf0f5801b806c15bf0a8bbec81
ez120 <ehsan.zand@gmail.com>
2020-02-27 14:37:20 +0330

    replacing lorem with robohash api

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = (props) => {
     return (
         <div class='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='robats' src={`http://lorempixel.com/150/200/sports/${props.id}`}/>
+            <img alt='robats' src={`https://robohash.org/${props.id}?size=200x200`}/>
             <h2>{props.name}</h2>
             <p>{props.email}</p>
         </div>
